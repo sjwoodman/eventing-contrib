b507bb14b9359258886f5759b4edfb7f565609ca
Matthias Wessendorf <mwessend@redhat.com>
2019-03-08 18:05:01 +0100

    adding routine

--- contrib/kafka/eventsource/cmd/eventsource/main.go
+++ contrib/kafka/eventsource/cmd/eventsource/main.go
@@ -108,12 +108,14 @@ func main() {
 				}
 
 				// post
-				posterr := postMessage(eventsourceconfig.KafkaTopic, eventsourceconfig.Target, msg, payload)
-				if posterr == nil {
-					consumer.MarkOffset(msg, "") // mark message as processed
-				} else {
-					log.Printf("Error posting message: %s", err)
-				}
+				go func() {
+					posterr := postMessage(eventsourceconfig.KafkaTopic, eventsourceconfig.Target, msg, payload)
+					if posterr == nil {
+						consumer.MarkOffset(msg, "") // mark message as processed
+					} else {
+						log.Printf("Error posting message: %s", err)
+					}
+				}()
 
 			}
 		case <-signals:
